ba6427a06952468d96bb92900d92b85d12c01817
Robbie Demaegdt <robbie.demaegdt@skyline.be>
2024-06-10 15:56:39 +0200

    Added new model

---
+++ ConnectorAPI.SkylineCommunications.SkylineLogicalLayer/InterAppMessages/MyMessages/StaticVariableModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Skyline.DataMiner.ConnectorAPI.SkylineCommunications.SkylineLogicalLayer.InterAppMessages.MyMessages
+{
+    using System;
+
+    /// <summary>
+    /// This is the model for the static variable
+    /// </summary>
+    [Serializable]
+    public class StaticVariableModel
+    {
+        /// <summary>
+        /// This is the name for the static variable.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// This is the value for the static variable.
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
